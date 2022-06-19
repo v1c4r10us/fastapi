7cf5cebeab033d47442700c72d49488fe43d706d
v1c4r10us <v1c4r10us.29@gmail.com>
2022-06-19 03:31:10 +0000

    Correct title

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from config.db import conn
 from docs import tags_metadata
 
 app = FastAPI(
-    title="RestAPI with python - 2022",
+    title="RestAPI with python",
     description="A simple API with fastapi & mongodb atlas",
     version="0.0.1",
     openapi_tags=tags_metadata
